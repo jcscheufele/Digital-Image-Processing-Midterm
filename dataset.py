957b1b18f2f83a6c7d35927649b53870636cf17f
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2022-02-19 12:25:09 -0500

    added network and dataset generators

--- dataset.py
+++ dataset.py
@@ -1,8 +1,22 @@
+import torch
+import datetime
+#import pytz
+#import pywt
+
+from torchvision.io import ImageReadMode
+from torchvision.io import read_image
+from torchvision import transforms
+
+from torch.utils.data import Dataset
+from torch import as_tensor, cat, flatten, div
+from matplotlib import pyplot as plt
+import os
+
 import cv2, os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from random import choices
+from random import choices, choice
 
 TRAIN_DATA = '../data/train'
 TEST_DATA = '../data/test'
@@ -41,22 +55,56 @@ def get_next_negative_path():
                     nightpath = TRAIN_DATA + "/" + local + "/" + night + "/" + nightimg
                     yield daypath, nightpath
 
-pos_counter = 0
-for img_paths in get_next_positive_path():
-    print(pos_counter, img_paths, 1)
-    pos_counter +=1
-    img = cv2.imread(img_paths[0])
-    img1 = cv2.imread(img_paths[1])
-    print(img.shape, img1.shape)
- 
+def createTransform():
+    p144 = (144, 256)
+    p240 = (240, 426)
+    p360 = (360, 640)
+    p480 = (480, 848)
+    crop = (355, 644)
+    listOfTransforms = [
+        transforms.Resize(p240)
+        ]
+    return transforms.Compose(listOfTransforms)
 
-'''neg_counter = 0
-for img_paths in get_next_negative_path():
-    print(neg_counter, img_paths, 0)
-    neg_counter +=1'''
+class BasicDataset(Dataset):
+    def __init__(self):
+        self.X = []
+        self.y = []
+        self.transform = createTransform()
+        self.makeXnY()
 
+    def __len__(self):
+        return len(self.y)
 
-'''for dir in test_dirs:
-    indv_dir = os.listdir(TEST_DATA + "/" + dir)
-    for img in indv_dir:
-        print(TEST_DATA + "/" + dir + "/" + img)'''
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+    def makeXnY(self):
+        print("Making Positives...")
+        counter = 0
+        for img_paths in get_next_positive_path():
+            print(f"img pair {counter}")
+            img1 = read_image(img_paths[0])
+            img2 = read_image(img_paths[1])
+
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+            self.X.append(img1, img2)
+            self.y.append(1)
+            counter += 1
+
+        print("Making Negatives...")
+        for img_paths in get_next_negative_path():
+            print(f"img pair {counter}")
+            img1 = read_image(img_paths[0])
+            img2 = read_image(img_paths[1])
+
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+            self.X.append(img1, img2)
+            self.y.append(0)
+            counter += 1
+        print("Completed")
+
--- network.py
+++ network.py
@@ -1,27 +1,149 @@
-import torch
-import cv2
-import numpy as np
-import datetime
-#import pytz
-#import pywt
-
-from torchvision.io import ImageReadMode
-from torchvision.io import read_image
+from collections import OrderedDict
+from torch import as_tensor, nn, no_grad, cat, flatten, float32, uint8, div, tensor
 from torchvision import transforms
+import numpy as np
+import wandb
+from random import sample
+import torch
 
-from torch.utils.data import Dataset
-from torch import as_tensor, cat, flatten, div
-from matplotlib import pyplot as plt
+class BasicNetwork(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(BasicNetwork,self).__init__()
+        self.wider_stack = nn.Sequential(OrderedDict([ #Creating an ordered dictionary of the 3 layers we want in our NN
+            ('Input', nn.Linear(in_features, 8192)),
+            ('Relu 1', nn.ReLU()),
+            ('Hidden Linear 1', nn.Linear(8192, 4096)),
+            ('Relu 2', nn.ReLU()),
+            ('Hidden Linear 2', nn.Linear(4096, 1024)),
+            ('Relu 3', nn.ReLU()),
+            ('Hidden Linear 3', nn.Linear(1024, 16)),
+            ('Relu 4', nn.ReLU()),
+            ('Output', nn.Linear(8, out_features))
+        ]))
+        self.day_convolution = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
 
-class BasicDataset(Dataset):
-    def __init__(self, data_dir):
-        self.data_dir = data_dir
+            nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128,128,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
 
-    def __len__(self):
-        return len(self.y)
-        
-    def getImage(self):
-        pass
+            nn.Conv2d(128,256,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256,256,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
 
-    def __getitem__(self, idx):
-        return self.X[idx], as_tensor(self.y[idx], dtype=torch.float32)
+            nn.Flatten(),
+            nn.Linear(814080,1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512,out_features)
+            )
+        self.night_convolution = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+
+            nn.Conv2d(64,128,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128,128,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+
+            nn.Conv2d(128,256,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256,256,kernel_size=3,stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+
+            nn.Flatten(),
+            nn.Linear(814080,1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512,out_features)
+            )
+    #Defining how the data flows through the layer, PyTorch will call this we will never have to call it
+    def forward(self, x):
+        logits1 = self.day_convolution(x[0])
+        logits2 = self.night_convolution(x[1])
+        logits3 = cat(logits1, logits2)
+        logits4 = self.wider_stack(logits3)
+        return logits4
+
+def train_loop(dataloader, model, loss_fn, optimizer, device, epoch, bs, will_save, key):
+    batches = int(len(dataloader.dataset)*.8/bs)
+    cumulative_loss = 0
+    ret = []
+
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X.to(device))
+        loss = loss_fn(pred, y.to(device))
+        cumulative_loss += loss
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if will_save and (batch % 10 == 0):
+            range = sample(list(np.arange(len(X))), min(len(X), 20))
+            for idx in range:
+                new = torch.reshape(X[idx][:-3], (50,2))
+                loc = X[idx][-3:]
+                save = {"Train Key": key, "Sample Epoch":epoch,"Sample Training Loss":loss,
+                "Diffs": new, 
+                "Sample Training Latitude":loc[0], "Sample Training Longitude":loc[1], "Sample Training Altitude":loc[2], 
+                "Sample Training Pred Lat": pred[idx][0].item(), "Sample Training Pred Lon": pred[idx][1].item(), "Sample Training Pred Alt": pred[idx][2].item(), 
+                "Sample Training Truth Lat": y[idx][0].item(), "Sample Training Truth Lon": y[idx][1].item(), "Sample Training Truth Alt": y[idx][2].item()}
+                ret.append(save)
+                key+=1
+            row = f"{epoch} [ {batch}/{batches} ] Loss: {loss} SAVED\n"
+            print(row)
+        else:
+            row = f"{epoch} [ {batch}/{batches} ] Loss: {loss}\n"
+            print(row)
+
+    averages_1 = f"End of Testing \n Test Error: \n Testing Avg loss: {(cumulative_loss/batches):>8f}\n"
+    print(averages_1)
+    return ret, cumulative_loss/batches, key
+
+#Runs through the whole dataset and gives final performace metrics
+def test_loop(dataloader, model, loss_fn, device, epoch, bs, will_save, key):
+    batches = int(len(dataloader.dataset)*.2/bs)
+    cumulative_loss = 0
+    ret = []
+
+    with no_grad():
+      for batch, (X, y) in enumerate(dataloader):
+        pred = model(X.to(device))
+        loss = loss_fn(pred, y.to(device))
+        cumulative_loss += loss
+        if will_save and (batch % 5 == 0):
+            range = sample(list(np.arange(len(X))), min(len(X), 20))
+            for idx in range:
+                new = torch.reshape(X[idx][:-3], (50,2))
+                loc = X[idx][-3:]
+                save = {"Test Key": key, "Sample Epoch": epoch, "Sample Testing Loss":loss,
+                "Diffs": new, 
+                "Sample Testing Latitude":loc[0], "Sample Testing Longitude":loc[1], "Sample Testing Altitude":loc[2], 
+                "Sample Testing Pred Lat": pred[idx][0].item(), "Sample Testing Pred Lon": pred[idx][1].item(), "Sample Testing Pred Alt": pred[idx][2].item(), 
+                "Sample Testing Truth Lat": y[idx][0].item(),   "Sample Testing Truth Lon": y[idx][1].item(),   "Sample Testing Truth Alt": y[idx][2].item()}
+                ret.append(save)
+                key+=1
+            row = f"{epoch} [ {batch}/{batches} ] Loss: {loss} SAVED\n"
+            print(row) 
+        else:
+            row = f"{epoch} [ {batch}/{batches} ] Loss: {loss}\n"
+            print(row)
+
+    averages_1 = f"End of Testing \n Test Error: \n Testing Avg loss: {(cumulative_loss/batches):>8f}\n"
+    print(averages_1)
+    return ret, cumulative_loss/batches, key
